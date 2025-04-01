a8ee7eeb5834c8b4c580f10fd696d3b7175700f7
AlexeiVorobyev <bopo6ey@gmail.com>
2025-03-31 22:44:36 -0700

    йф

---
+++ src/test/java/tests/MainTest.java
@@ -0,0 +1,30 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class MainTest {
+    @Test
+    public void testFirst() {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.instagram.com/");
+
+        WebElement loginField = driver.findElement(By.xpath("/html/body/div[1]/div/div/div[2]/div/div/div[1]/div[1]/div/section/main/article/div[2]/div[1]/div[2]/div/form/div[1]/div[1]/div/label/input"));
+        WebElement passwordField = driver.findElement(By.xpath("/html/body/div[1]/div/div/div[2]/div/div/div[1]/div[1]/div/section/main/article/div[2]/div[1]/div[2]/div/form/div[1]/div[2]/div/label/input"));
+
+        loginField.sendKeys("ivan_shchetkin_zarya");
+        passwordField.sendKeys("spokesilver0711");
+
+        WebElement SubmitButton = driver.findElement(By.xpath("/html/body/div[1]/div/div/div[2]/div/div/div[1]/div[1]/div/section/main/article/div[2]/div[1]/div[2]/div/form/div[1]/div[3]/button"));
+
+        SubmitButton.click();
+
+        driver.quit();
+
+    }
+
+}
