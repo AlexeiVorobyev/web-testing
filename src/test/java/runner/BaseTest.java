a255f5025dfa5103e106ab31b4a206cc578bad89
AlexeiVorobyev <bopo6ey@gmail.com>
2025-03-31 23:16:13 -0700

    BaseTest

---
+++ src/test/java/runner/BaseTest.java
@@ -0,0 +1,57 @@
+package runner;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public class BaseTest {
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver(buildChromeOptions());
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("https://automationexercise.com/");
+    }
+
+
+    private ChromeOptions buildChromeOptions() {
+        ChromeOptions options = new ChromeOptions();
+        String ci = System.getenv("CI");
+        String chromeOpts = System.getenv("CHROME_OPTIONS");
+        if ( ci != null && ci.equals("true")) {
+            options.addArguments(chromeOpts.split("\\s+"));
+        }
+        else {
+            options.addArguments("--window-size=1920x1080");
+        }
+        return options;
+    }
+
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+
+    public WebDriverWait wait(int seconds) {
+        return new WebDriverWait(getDriver(), Duration.ofSeconds(seconds));
+    }
+}
+
--- src/test/java/tests/MainTest.java
+++ src/test/java/tests/MainTest.java
@@ -13,8 +13,8 @@ public class MainTest {
 
         driver.get("https://www.instagram.com/");
 
-        WebElement loginField = driver.findElement(By.xpath("/html/body/div[1]/div/div/div[2]/div/div/div[1]/div[1]/div/section/main/article/div[2]/div[1]/div[2]/div/form/div[1]/div[1]/div/label/input"));
-        WebElement passwordField = driver.findElement(By.xpath("/html/body/div[1]/div/div/div[2]/div/div/div[1]/div[1]/div/section/main/article/div[2]/div[1]/div[2]/div/form/div[1]/div[2]/div/label/input"));
+        WebElement loginField = driver.findElement(By.name("username"));
+        WebElement passwordField = driver.findElement(By.name("password"));
 
         loginField.sendKeys("ivan_shchetkin_zarya");
         passwordField.sendKeys("spokesilver0711");
